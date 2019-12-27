717b23f96b3b7720c19f990bf513c04dbe04dfaa
Peter Benjamin <petermbenjamin@gmail.com>
2019-12-27 13:54:29 -0800

    [Fix] Fix go path

--- cmd/go-open/main.go
+++ cmd/go-open/main.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"os"
 
-	gopen "github.com/petermbenjamin/go-open"
+	open "github.com/pbnj/go-open"
 )
 
 func main() {
 	for _, arg := range os.Args[1:] {
-		if err := gopen.Open(arg); err != nil {
+		if err := open.Open(arg); err != nil {
 			fmt.Println(err)
 		}
 	}
